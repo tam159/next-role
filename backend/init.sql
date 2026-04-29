025e3a3dc6b792523aa3f0efdeebeb0d46aabd67
Tam Nguyen <npt.dc@outlook.com>
2026-04-29 12:19:08 +0700

    feat: initialize backend

---
+++ backend/init.sql
@@ -0,0 +1,5 @@
+-- Initialize pgvector extension
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Create indexes will be handled by SQLModel/Alembic migrations
+-- This file is for basic PostgreSQL initialization only
